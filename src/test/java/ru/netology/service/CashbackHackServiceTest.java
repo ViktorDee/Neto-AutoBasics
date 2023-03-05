1a5cad5a969cf13c9f01cf8ff733bbae5b221ac2
ViktorD <vdimitrenko96@gmail.com>
2023-03-05 18:04:55 +0700

    minor changes in Test class

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -10,10 +10,10 @@ public class CashbackHackServiceTest {
     public void shouldCountRemain() {
         CashbackHackService service = new CashbackHackService();
 
-        int actual = service.remain(2450);
-        int expected = 550;
+        int actual = service.remain(1000);
+        int expected = 0;
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
     @Test
@@ -23,7 +23,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(0);
         int expected = 1000;
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
     @Test
@@ -33,7 +33,7 @@ public class CashbackHackServiceTest {
         int actual = service.remain(2999);
         int expected = 1;
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
     @Test
@@ -43,6 +43,6 @@ public class CashbackHackServiceTest {
         int actual = service.remain(3001);
         int expected = 999;
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 }
